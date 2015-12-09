f600b9a088b23e3206e7d53787f4da1e7601179a
prakash khadka <p.khadka56@yahoo.com>
2015-12-09 16:47:11 +0545

    Update ip2host.py

--- ip2host.py
+++ ip2host.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python
 import socket
+
+def check_ip():
+	try:
+		socket.inet_aton(IP) # check for valid ipv4 address
+	except Exception, e:
+		print "Given list do not have valid list of ipv4 addresses."
+		print "Invalid ip addresss = " + str(IP)
+		exit()
+
 with open('ip.txt') as ipfile:
 	for IP in ipfile:
-		try:
-			host = socket.getfqdn(IP)
-			print host, IP.strip()
-		except Exception, e:
-			print e
+		check_ip()
+		host = socket.getfqdn(IP)
+		print host, IP.strip()
