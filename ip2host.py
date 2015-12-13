67215123ccadf9397107e5baecec15d472999c02
prakash khadka <p.khadka56@yahoo.com>
2015-12-13 14:52:10 +0545

    Update ip2host.py
    Fixed two or more ip per line.

--- ip2host.py
+++ ip2host.py
@@ -1,22 +1,28 @@
 #!/usr/bin/python
+
 import socket
 import sys
 
+BLUE='\033[34m'
+RED='\033[31m'
+NORMAL='\033[0m'
+
 def check_ip():
-	try:
-		socket.inet_aton(ip)
-	except Exception, e:
-		print "Given list do not have valid list of ipv4 addresses."
-		print "Invalid ip addresss = " + str(ip)
-		exit()
+        try:
+                socket.inet_aton(ip)
+        except Exception, e:
+                print RED+"Given list do not have valid list of ipv4 addresses."
+                print "Invalid ip addresss = "+NORMAL + str(ip)
+                exit()
 
 file = sys.argv[1]
 with open(file) as ipfile:
-	for IP in ipfile:
-		ip = IP.rstrip('\r\n')
-		if ip == '':
-			pass
-		else:
-			check_ip()
-			host = socket.getfqdn(ip)
-			print host, ip
+        for iplist in ipfile:
+                for IP in iplist.split():
+                        ip = IP.strip(' \t\n\r')
+                        if ip == '':
+                                pass
+                        else:
+                                check_ip()
+                                host = socket.getfqdn(ip)
+                                print BLUE+ip+NORMAL + " hostname is " + BLUE+host+NORMAL
